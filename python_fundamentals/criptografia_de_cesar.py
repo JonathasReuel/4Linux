6ad4bc539d46de3f56187997d708a9e4291e05d7
JonathasReuel <jonathas.reuel@clickbus.com>
2018-10-16 11:08:43 -0300

    Substituição de Ifs por Dicts

--- python_fundamentals/criptografia_de_cesar.py
+++ python_fundamentals/criptografia_de_cesar.py
@@ -5,36 +5,47 @@ import sys
 alfabeto = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','y','x','z']
 tam = len(alfabeto) - 1
 
-def criptografar(message):
+def criptografar(message, quantidade):
 	nova_palavra = ''
 	for i in message:
-		if (alfabeto.index(i) + 3) > tam:
-			j = (alfabeto.index(i) + 3) - tam
+		if (alfabeto.index(i) + quantidade) > tam:
+			j = (alfabeto.index(i) + quantidade) - tam
 			nova_palavra += alfabeto[j]
 		else:
-			nova_palavra += alfabeto[alfabeto.index(i) + 3]
+			nova_palavra += alfabeto[alfabeto.index(i) + quantidade]
 
-	return print(nova_palavra)
+	return print('Palavra Criptografada: %s' % nova_palavra)
 
-def decriptografar(message):
+def decriptografar(message, quantidade):
 	nova_palavra = ''
 	for i in message:
-		if (alfabeto.index(i) - 3) <= 0:
-			j = tam + (alfabeto.index(i) - 3)
+		if (alfabeto.index(i) - quantidade) < 0:
+			j = tam + (alfabeto.index(i) - quantidade)
 			nova_palavra += alfabeto[j]
+		elif (alfabeto.index(i) - quantidade) == 0:
+			nova_palavra += alfabeto[0]
 		else:
-			nova_palavra += alfabeto[alfabeto.index(i) - 3]
-	return print(nova_palavra)
+			nova_palavra += alfabeto[alfabeto.index(i) - quantidade]
+	return print('Palavra Decriptografada: %s' % nova_palavra)
 
 def main():
-	command = sys.argv[1].lower()
-	message = sys.argv[2].lower()
-	if command == 'criptografar':
-		criptografar(message)
-	elif command == 'decriptografar':
-		decriptografar(message)
+	if len(sys.argv) == 1:
+		print('''O script requer dois parâmetros obrigatórios:
+				\n-criptografar/decriptografar e a palavra para criptografia/decriptografia.
+				\nTambém pode ser utilizado o parâmetro opcional de quantidade de letras
+				\npara utilizar nos processos criptográficos.''')
 	else:
-		print('Comando não reconhecido!')
+		command = sys.argv[1].lower()
+		message = sys.argv[2].lower()
+
+		if len(sys.argv) < 4:
+			qtd = 3
+		else:
+			qtd = int(sys.argv[3])
+
+		comandos = {'criptografar':criptografar,'decriptografar':decriptografar}
+
+		comandos[command](message,qtd)
 
 if __name__ == '__main__':
 	main()
